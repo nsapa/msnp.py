4a936eaebed79a636c3e6be91bd73b83621ebd53
Manish Jethani <manish_jethani@yahoo.com>
2004-01-15 18:36:59 +0100

    Import msnp.py version 0.4.1
    This release fixes critical issues in the friend list management functions.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!python
+
+from distutils.core import setup
+
+setup(name="msnp.py",
+      version="0.4.1",
+      description="An implementation of the MSN Messenger Protocol",
+      license="GNU General Public License (Version 2)",
+      author="Manish Jethani",
+      author_email="manish_jethani AT yahoo.com",
+      url="http://msnp.sf.net/",
+      packages = ['msnp'],
+     )
+
